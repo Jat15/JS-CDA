e00b3fd99d0469641e78a21ea9b105fea47084fb
Jat15 <jinan15@hotmail.com>
2020-07-13 00:20:42 +0200

    vs.js
    - Importation du formulaire
    
    form_name
    - Engistrement de l'id
    - Le message d'erreur du pattern et cherche dans le js formulaire si non défini
    
    add_form
    - Vérification si il faut pas crée un event pour un input
    
    modal_resul
    - supression de bouton (inutile)

--- js/vs.js
+++ js/vs.js
@@ -1,3 +1,7 @@
+import * as verif_form from './formulaire.js'
+
+let id_list = []
+
 //export la function de création de input (fragment)
 export function form_name(data) {
 
@@ -9,12 +13,13 @@ export function form_name(data) {
 
     el = document.createElement("input");
     el.className =  "mdl-textfield__input";
-    if (data.pattern){
+    if (data.pattern) {
         let att = document.createAttribute("data-vs-form-pattern")
-        att.value = data.pattern;  
+        att.value = data.pattern;
         el.setAttributeNode(att);
+        id_list.push(data.id);
     }
-    el.id = data.id ;
+    el.id = data.id;
     el.type = "text";
     fragment.querySelector('div').appendChild(el);
 
@@ -26,7 +31,7 @@ export function form_name(data) {
 
     el = document.createElement("span");
     el.className = "mdl-textfield__error";
-    el.textContent = data.erreur;
+    el.textContent = data.erreur ? data.erreur : verif_form.regex[data.pattern].message; 
     fragment.querySelector('div').appendChild(el);
 
     return fragment;
@@ -38,7 +43,6 @@ export function form_radio(data) {
     let create= "<fieldset class=\"mdl-cell mdl-cell--12-col\" ><legend>" + data.box_name+ "</legend>"
     let id
     let n = 0
-
 
     data.value.forEach(function(value){
        id = data.name + "_" + n;
@@ -57,19 +61,21 @@ export function form_radio(data) {
 
 //Création du bouton
 export function form_button(data) {
-    let fragment = document.createDocumentFragment();   
-    let el = document.createElement("button");
-    el.className =  "mdl-button mdl-js-button mdl-button--raised mdl-button--colored";
+    let fragment = document.createDocumentFragment()
+    let el = document.createElement("button")
+    el.className =  "mdl-button mdl-js-button mdl-button--raised mdl-button--colored"
     el.type = "button"
-    el.id = data.id;
-    el.textContent = data.name;
-    fragment.appendChild(el);
-
+    el.id = data.id
+    el.textContent = data.name
+    fragment.appendChild(el)
 
     return fragment
 }
 
-//export la function pour ajouter du code html grâce a un selecteur
+/*
+    export la function pour ajouter du code html grâce a un selecteur
+    Création de l'event pour le input
+*/
 export function add(data) {
     if (typeof data.text === 'string')
         document.querySelector(data.selecteur).insertAdjacentHTML('beforeend', data.text);
@@ -77,18 +83,24 @@ export function add(data) {
         document.querySelector(data.selecteur).appendChild(data.text);
 
     componentHandler.upgradeDom()
+
+    let new_table_id = []
+    id_list.forEach (
+        function(id) {
+            document.getElementById(id) ? verif_form.create_event(id) : new_table_id.push(id)
+        }
+    )
+    id_list = new_table_id
 }
 
 /*
     Supprime tout les enfant d'un parent du Dom
 */
 export function kill_child(selecteur) {
-
     let child = selecteur.childNodes;
     for (let i=child.length-1; i >= 0; i--) {
         selecteur.childNodes[i].remove()
     }
-
 }
 
 /*
@@ -157,7 +169,7 @@ export function form_start(data){
 
     Actualisation de mdl........
 */
-export function modal_result(bouton, message) {
+export function modal_result(message) {
     kill_child(document.querySelector("#resultat"))
 
     document.querySelector("#resultat").insertAdjacentHTML('beforeend', message)
