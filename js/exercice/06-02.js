428dd4ceb05f18e1717bdf326ad11cc516f1ceeb
Jat15 <jinan15@hotmail.com>
2020-07-13 00:25:54 +0200

    06-02.js
    - Mise en forme du code
    - Gestion des erreurs
    - Ajout de text pour les valeurs inadéquate

--- js/exercice/06-02.js
+++ js/exercice/06-02.js
@@ -1,5 +1,6 @@
 import {} from '../menu.js'
 import * as vs from '../vs.js'
+import * as verif_form from '../formulaire.js'
 
 const def = { 
     cours : "06 - Conditions",
@@ -13,36 +14,50 @@ export function vue() {
         annees : {
             id : "annees",
             name : "Année de naissance",
-            erreur : "Ceci n'est pas une année!"
+            pattern: "entier"
         }
     }
 
     vs.form_start(def)
 
-    vs.add(
-        {
-            selecteur : "#formulaire",
-            text : vs.form_name (data_send.annees)
-        }
-    )
+    vs.add({
+        selecteur : "#formulaire",
+        text : vs.form_name (data_send.annees)
+    })
 
     vs.form_end()
 
-
     document.getElementById('valid_form').addEventListener("click", function() {
+        let resultat
 
-        let majeur
-        const ladate = new Date()
-        let nombres =  this.parentNode.querySelector("#" + data_send.annees.id).value
-        let age = ladate.getFullYear() - nombres
+        if (verif_form.no_error()) {
+            let majeur
+            const ladate = new Date()
+            let nombres =  this.parentNode.querySelector("#" + data_send.annees.id).value
+            let age = ladate.getFullYear() - nombres
 
-        majeur = age >= 18 ? "majeur": "mineur"
-    
-       const resultat = "<p>Tu as "+ (age) +" ans, donc tu est "+ majeur + "</p>"
+            majeur = age >= 18 ? "majeur": "mineur"
+        
+            resultat = "<p>Tu as "+ (age) +" ans, donc tu est "+ majeur + "</p>"
 
 
-       vs.modal_result(this, resultat)
+            if (age < 0){
+                resultat += "Tu viens du futur ?"
+            } else if  (age == 0){
+                resultat += "Tes nées aujourd'hui !"
+            } else if (age <= 3) {
+                resultat += "Tes pas un peu jeune pour regarder des sites ?"
+            } else if (age >= ladate.getFullYear()) {
+                resultat += "Ta connu J.C. !"
+            } else if (age >= 200) {
+                resultat += "Tu peu encore lire !"
+            }
+        } else {
+            resultat = "<p>Remplissez correctement le champ</p>"
+        }
 
-    }, false);
+        vs.modal_result(resultat)
+
+    }, false)
 
 }
