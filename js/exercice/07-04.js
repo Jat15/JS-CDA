d1a71f0a741e9c5b8d225d52304e0c8daaad9d2a
Jat15 <jinan15@hotmail.com>
2020-07-13 00:29:59 +0200

    07-04.js
    - Mise en forme du code
    - Gestion des erreurs
    - Gestion de l'ordres des nombres

--- js/exercice/07-04.js
+++ js/exercice/07-04.js
@@ -1,5 +1,6 @@
 import {} from '../menu.js'
 import * as vs from '../vs.js'
+import * as verif_form from '../formulaire.js'
 
 const def = { 
     cours : "07 - Boucles",
@@ -12,46 +13,55 @@ export function vue() {
         nombre1: {
             id: "nombre1",
             name: "Nombre",
-            erreur: "Nom pas bon !"
+            pattern : "entier"
         },
         nombre2: {
             id: "nombre2",
             name: "Nombre",
-            erreur: "Nom pas bon !"
+            pattern: "entier"
         }
     }
 
     vs.form_start(def)
 
-    vs.add(
-        {
-            selecteur: "#formulaire",
-            text: vs.form_name(data_send.nombre1)
-        }
-    )
+    vs.add({
+        selecteur: "#formulaire",
+        text: vs.form_name(data_send.nombre1)
+    })
 
-    vs.add(
-        {
-            selecteur: "#formulaire",
-            text: vs.form_name(data_send.nombre2)
-        }
-    )
+    vs.add({
+        selecteur: "#formulaire",
+        text: vs.form_name(data_send.nombre2)
+    })
 
     vs.form_end()
 
     document.getElementById('valid_form').addEventListener("click", function () {
-        let resultat = 0;
-        const nombre1 = parseInt(this.parentNode.querySelector("#" + data_send.nombre1.id).value);
-        const nombre2 = parseInt(this.parentNode.querySelector("#" + data_send.nombre2.id).value);
+        let message = ""
 
+        if (verif_form.no_error()) {
+            let resultat = 0
+            let nombre1 = parseInt(this.parentNode.querySelector("#"+data_send.nombre1.id).value)
+            let nombre2 = parseInt(this.parentNode.querySelector("#"+data_send.nombre2.id).value)
 
-        for (let i = 0; i < (nombre2 - nombre1)+1; i++)
-        {   
-            resultat = resultat + (nombre1+i);
+            if (nombre1 > nombre2) {
+                const mem = nombre1
+                nombre1 = nombre2
+                nombre2 = mem
+            } else if (nombre1 === nombre2){
+               message = "Pas vraiment utile..." 
+            }
+
+            for (let i = 0 ; i < (nombre2 - nombre1) + 1 ; i++) {   
+                resultat = resultat + (nombre1+i)
+            }
+
+            message += "<p>" + resultat + "</p>"
+        } else {
+            message = "<p>Remplissez correctement les champs</p>"
         }
 
-        resultat = "<p>" + resultat + "</p>"
-        vs.modal_result(this, resultat)
+        vs.modal_result(message)
 
-    }, false);
+    }, false)
 }
